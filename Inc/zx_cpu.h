6af806356bec67bae20c5960121a6b4f659f3aef
Dmitry Anokhin <anokhindm@gmail.com>
2018-10-11 00:19:40 +0300

    CPU memory read\write functions

--- Inc/zx_cpu.h
+++ Inc/zx_cpu.h
@@ -8,6 +8,7 @@
 #ifndef ZX_CPU_H_
 #define ZX_CPU_H_
 
+#include<stdint.h>
 
 
 #endif /* ZX_CPU_H_ */
--- Src/zx_cpu.c
+++ Src/zx_cpu.c
@@ -6,3 +6,35 @@
  */
 
 
+#include "zx.h"
+#include "zx_cpu.h"
+
+uint8_t zx_read8(ZX* zx, uint16_t addr) {
+	if(addr < 16384) {
+		return zx->rom[addr];
+	} else {
+		return zx->ram[addr-16384];
+	}
+}
+
+uint16_t zx_read16(ZX* zx, uint16_t addr) {
+	if(addr < 16384) {
+		return ((uint16_t*)&zx->rom[addr])[0];
+	} else {
+		return ((uint16_t*)&zx->ram[addr-16384])[0];
+	}
+}
+
+void zx_write8(ZX* zx, uint16_t addr, uint8_t data) {
+	if(addr >= 16384) {
+		zx->ram[addr-16384] = data;
+	}
+}
+
+void zx_write16(ZX* zx, uint16_t addr, uint16_t data) {
+	if(addr >= 16384) {
+		((uint16_t*)&zx->ram[addr-16384])[0] = data;
+	}
+}
+
+
